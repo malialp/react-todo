08d008cefb4dfd78798eda0c0be068b2c42ff08b
AliBaba05 <malibaba2234@gmail.com>
2023-06-27 01:29:02 +0300

    create redux

---
+++ src/Store/slices/todoSlice.js
@@ -0,0 +1,43 @@
+import { createSlice, current } from "@reduxjs/toolkit";
+
+const initTodos = () => {
+  const todos = JSON.parse(localStorage.getItem("todos"));
+  if (todos) {
+    return { value: todos };
+  }
+  return { value: [] };
+};
+
+export const todoSlice = createSlice({
+  name: "todos",
+  initialState: initTodos(),
+  reducers: {
+    addTodo: (state, action) => {
+      state.value.unshift(action.payload.todo);
+      state.value = state.value.sort((a, b) => a.status - b.status);
+
+      localStorage.setItem("todos", JSON.stringify(state.value));
+    },
+    deleteTodo: (state, action) => {
+      state.value = state.value
+        .filter((todo) => todo.id !== action.payload.id)
+        .sort((a, b) => a.status - b.status);
+
+      localStorage.setItem("todos", JSON.stringify(state.value));
+    },
+    completeTodo: (state, action) => {
+      state.value = state.value
+        .map((todo) =>
+          todo.id === action.payload.id
+            ? { ...todo, status: !todo.status }
+            : todo
+        )
+        .sort((a, b) => a.status - b.status);
+
+      localStorage.setItem("todos", JSON.stringify(state.value));
+    },
+  },
+});
+
+export const { addTodo, deleteTodo, completeTodo } = todoSlice.actions;
+export default todoSlice.reducer;
