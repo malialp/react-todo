466604c27ee1186f4675cf9260f26a9c03c561f5
AliBaba05 <malibaba2234@gmail.com>
2023-06-27 20:55:20 +0300

    edit styles

--- src/App.jsx
+++ src/App.jsx
@@ -32,31 +32,31 @@ const App = () => {
   };
 
   return (
-    <div className="container mx-auto h-full flex flex-col">
-      <div className="flex flex-row items-center p-4 gap-4 ">
+    <div className="container mx-auto flex h-full flex-col">
+      <div className="flex flex-row items-center gap-4 p-4 ">
         <button
           onClick={() => {
             setIsSettingsOpen(true);
           }}
-          className="border-2 border-defaultBorder p-2 rounded-lg bg-defaultBG hover:bg-hoverBG outline-none focus:border-focusBorder duration-300"
+          className="rounded-lg border-2 border-light-border p-2 outline-none  duration-300 hover:bg-light-background-2  focus:border-light-border-focus dark:border-dark-border dark:bg-dark-background-2 dark:text-light-text dark:hover:bg-dark-hover-background dark:focus:border-dark-border-focus"
         >
-          <FiSettings className="text-light h-auto w-[22px]" />
+          <FiSettings className="h-auto w-[22px]" />
         </button>
         <input
-          className="border-2 border-defaultBorder p-2 rounded-lg bg-defaultBG hover:bg-hoverBG flex-1 text-light outline-none focus:border-focusBorder duration-300"
+          className="flex-1 rounded-lg border-2 border-light-border p-2 outline-none duration-300 hover:bg-light-background-2 focus:border-light-border-focus dark:border-dark-border dark:bg-dark-background-2 dark:text-light-text dark:hover:bg-dark-hover-background dark:focus:border-dark-border-focus"
           type="text"
           placeholder="Search"
         />
-        <button className="border-2 border-defaultBorder p-2 rounded-lg bg-defaultBG hover:bg-hoverBG outline-none focus:border-focusBorder duration-300">
-          <BsThreeDotsVertical className="text-light h-auto w-[22px]" />
+        <button className="rounded-lg border-2 border-light-border p-2 outline-none duration-300 hover:bg-light-background-2 focus:border-light-border-focus dark:border-dark-border dark:bg-dark-background-2 dark:text-light-text dark:hover:bg-dark-hover-background dark:focus:border-dark-border-focus">
+          <BsThreeDotsVertical className="h-auto w-[22px]" />
         </button>
       </div>
-      <div className="flex flex-col p-4 h-full overflow-y-auto gap-4">
+      <div className="flex h-full flex-col gap-4 overflow-y-auto p-4">
         <button
           onClick={() => {
             setIsNewTodoOpen(true);
           }}
-          className="border-2 border-defaultBorder hover:bg-defaultBG outline-none focus:border-focusBorder text-center text-light text-xl font-semibold rounded-lg p-4 duration-300"
+          className="rounded-lg border-2 border-light-border bg-transparent p-4 text-center text-xl font-semibold outline-none duration-300 hover:bg-light-background-2 focus:border-light-border-focus dark:border-dark-border dark:text-light-text dark:hover:bg-dark-hover-background dark:focus:border-dark-border-focus dark:focus:bg-dark-background-2"
         >
           New Todo
         </button>
@@ -67,40 +67,40 @@ const App = () => {
                 key={todo.id}
                 className={`${
                   todo.status && "opacity-50"
-                } bg-defaultBG border-2 border-defaultBorder rounded-lg mb-4 p-3 flex flex-row gap-4`}
+                } mb-4 flex flex-row gap-4 rounded-lg border-2 border-light-border bg-light-background p-3 dark:border-dark-border dark:bg-dark-background-2`}
               >
-                <div className="flex flex-col gap-2 flex-1 justify-evenly">
+                <div className="flex flex-1 flex-col justify-evenly gap-2">
                   <h1
-                    className={`text-light text-[22px] font-semibold ${
+                    className={`text-[22px] font-semibold text-dark-text dark:text-light-text ${
                       todo.status && "line-through"
                     }`}
                   >
                     {todo.title}
                   </h1>
                   <p
-                    className={`text-semiLight text-[16px] break-all ${
+                    className={`break-all text-[16px] text-dark-text-2 dark:text-light-text-2 ${
                       todo.status && "line-through"
                     }`}
                   >
                     {todo.description}
                   </p>
                 </div>
-                <div className="flex flex-col justify-center items-center gap-4">
+                <div className="flex flex-col items-center justify-center gap-4">
                   <button
                     onClick={() => {
                       handleComplete(todo.id);
                     }}
-                    className="border-2 border-defaultBorder p-2 rounded-lg bg-defaultBG hover:bg-green outline-none focus:border-focusBorder duration-300"
+                    className="rounded-lg border-2 p-2 outline-none duration-300 hover:bg-btn-green hover:text-light-text focus:border-light-border-focus focus:bg-btn-green focus:text-light-text dark:border-dark-border dark:text-light-text dark:focus:border-dark-border-focus"
                   >
-                    <FaCheck className="text-light" />
+                    <FaCheck />
                   </button>
                   <button
                     onClick={() => {
                       handleDelete(todo.id);
                     }}
-                    className="border-2 border-defaultBorder p-2 rounded-lg bg-defaultBG hover:bg-red outline-none focus:border-focusBorder duration-300"
+                    className="rounded-lg border-2 p-2 outline-none duration-300 hover:bg-btn-red hover:text-light-text focus:border-light-border-focus focus:bg-btn-red focus:text-light-text dark:border-dark-border dark:text-light-text dark:focus:border-dark-border-focus"
                   >
-                    <FaTrashCan className="text-light" />
+                    <FaTrashCan />
                   </button>
                 </div>
               </div>
